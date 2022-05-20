869f80a2624f9b78b072bfa7bfd909efc48bfe58
ba825894@wellingtoncdsb.ca <ba825894@wellingtoncdsb.ca>
2022-05-20 17:01:47 +0000

    number 3

--- UBMP4-Intro-4-Functions.X/Intro-4-Functions.c
+++ UBMP4-Intro-4-Functions.X/Intro-4-Functions.c
@@ -98,13 +98,17 @@ int main(void)
 /* Program Analysis
  * 
  * 1.   Which function in this program will run first? How do you know?
- * 
+ * The main functions will be the first thing that will run first. This is because the UBMP4 will search for the main function when it starts up.
+
+
  * 2.   What is the purpose of the 'unsigned char' variable type declaration in
  *      the button_pressed() function? Is it used by this function to receive
  *      a variable from, or return a variable to the main code?
- * 
+ * The purpose of the 'unsigned char' in the button_pressed() is to give it a value when asked. This function is used to return a variable to the main code.
+
  * 3.   How does the function call statement 'button = button_pressed();' in the
  *      main code support your answer in 2, above?
+ * This support my answer because button and button_pressed are similar
  * 
  * 4.   What is the purpose of the 'unsigned char' variable type declaration in
  *      the pwm_LED5() function? Where does the value of the variable come from?
